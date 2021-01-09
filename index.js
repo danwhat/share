99bc67ae033e535eda6c4cf5a6bf7109c29f450d
Danwhat <daanwhat@gmail.com>
2021-01-09 20:06:19 -0300

    Esquecemos do Cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes')
 const port = process.env.PORT || 3000
 const app = express();
 
+app.use(cors());
 app.use(express.json())
 app.use(routes);
 
