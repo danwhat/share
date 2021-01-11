ed78ff421bcd2cb30085681f5a785018c6435e3a
Danwhat <daanwhat@gmail.com>
2021-01-11 14:25:18 -0300

    Deixa eu ver uma parada aqui.

--- getMessages.js
+++ getMessages.js
@@ -3,7 +3,7 @@ module.exports = (req, res) => {
   const Message = require('./messageSchema')
   const quantMessages = require('./index')
 
-  Message.find().sort({'date': -1}).limit(30).exec((err, Message) => {
+  Message.find().sort({'date': 1}).limit(30).exec((err, Message) => {
     if (err) return console.error(err);
     res.status(200).send({ messages: Message, total: quantMessages.quantMessages.quantity })
   })
